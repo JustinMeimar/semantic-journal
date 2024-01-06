263bbe860e67415e02cf41cc9aa7eaf07f76fc59
luke3 <luke3@ualberta.ca>
2024-01-06 15:01:19 -0700

    change to a single text box

--- app/src/pages/Journal.jsx
+++ app/src/pages/Journal.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-function Journal() {
-    return (
-        <h1>this is the journal</h1>
-    );
-} export default Journal;
+class Journal extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            goal: {metrics: ["Running speed", "Heart rate", "Distance"]}
+        };
+    }
+
+    render() {
+
+        return (
+            <div className="entryPage">
+                <p className="entryHeader">
+                    {`Your three metrics are: ${this.state.goal.metrics.join(", ")}`}
+                </p>
+                <div className="entryDiv">
+                    <p htmlFor="otherComments" className="entryHeader">How did today go?</p>
+                    <textarea type="text" id="mainEntry" className="entryPrompt" />
+                </div>
+                <button className="entrySubmitButton">Record</button>
+            </div>
+        )
+    }
+}
+export default Journal;
