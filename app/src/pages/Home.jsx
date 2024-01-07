e3586a05b768dbdf51615798113ec9b35a7ebf55
luke3 <luke3@ualberta.ca>
2024-01-07 08:56:37 -0700

    fill graph with proper data

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
+const urlBase = "http://127.0.0.1:5000"
+
+const fetchJournals = async () => {
+    try {
+        const response = await fetch(`${urlBase}/get_journals`);
+        const journalsJSON = await response.json();
+        return journalsJSON;
+    } catch (error) {
+        console.log("Error:", error);
+        return [];
+    }
+}
+
 const Home = () => {
     const [goal, setGoal] = useState('');
     const chartRef = useRef(null);
+
+    const [journalContent, setJournalContent] = useState("");
+    const [journals, setJournals] = useState([]);
+
     const [metrics, setMetrics] = useState({
         "metric-1": "metric-1",
         "metric-2": "metric-2",
@@ -76,6 +93,96 @@ const Home = () => {
             });
         }
     }, [metrics]);
+
+    useEffect(() => {
+        const getJournals = async () => {
+            const journalsData = await fetchJournals();
+            if (journalsData && journalsData.length > 0) {
+                setJournalContent(journalsData[0].content);
+            } else {
+                setJournalContent("No journals available.");
+            }
+            setJournals(journalsData);
+
+            let data0 = [];
+            let data1 = [];
+            let data2 = [];
+            let labels = [];
+
+            for (let i = 0; i < journalsData.length; i++) {
+                data0.push(journalsData[i].quantities[0]);
+                data1.push(journalsData[i].quantities[1]);
+                data2.push(journalsData[i].quantities[2]);
+                labels.push("");
+            }
+
+            if (chartRef.current) {
+
+                const ctx = chartRef.current.getContext('2d');
+                
+                if (chartInstanceRef.current) {
+                    chartInstanceRef.current.destroy();
+                }
+
+                chartInstanceRef.current = new Chart(ctx, {
+                    type: "line",
+                    data: {
+                        labels: labels,
+                        datasets: [{ 
+                            data: data0,
+                            label: metrics["metric-1"], 
+                            borderColor: "#3e95cd",
+                            backgroundColor: "#7bb6dd",
+                            fill: false,
+                        }, { 
+                            data: data1,
+                            label: metrics["metric-2"], 
+                            borderColor: "#3cba9f",
+                            backgroundColor: "#71d1bd",
+                            fill: false,
+                        }, { 
+                            data: data2,
+                            label: metrics["metric-3"], 
+                            borderColor: "#ffa500",
+                            backgroundColor:"#ffc04d",
+                            fill: false,
+                        },
+                        ]
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    color: '#ffffff'
+                                }
+                            }
+                        },
+                        scales: {
+                            x: {
+                                ticks: {
+                                    color: '#ffffff'
+                                },
+                                grid: {
+                                    color: '#666'
+                                }
+                            },
+                            y: {
+                                ticks: {
+                                    color: '#ffffff'
+                                },
+                                grid: {
+                                    color: '#666'
+                                }
+                            }
+                        },
+                        maintainAspectRatio: true,
+                    }
+                });
+            }
+
+        };
+        getJournals();
+    }, []);
 
     const getMetrics = (event) => {
         event.preventDefault();
