98ea7f74578d3b0b51092a218b8f48b21195bb51
Kevin Chen <kkcc.chen@gmail.com>
2024-01-07 09:40:51 -0700

    3 zeros

--- backend/quantify_goals.py
+++ backend/quantify_goals.py
@@ -12,6 +12,8 @@ def init_chat(goals_dict):
     You are a goal helper trying to evaluate quantitatively how well the user has performed on three goals. You need to generate 3 numbers ranging from 0 to 10, where 10 is completely meeting the goal an 0 is doing the opposite of the goal, respectively for the following 3 goals, and increments of 0.5: \n 1. {goals_list[0]} \n 2. {goals_list[1]} \n 3. {goals_list[2]}. Return the three numbers, with just the numbers, one on each new line, like this: 
 
     '0.5\n5.0\n9.5'
+
+    You must output three numbers. If the input is unrelated give three zeros.
     """
     return [{'role': 'system', 'content': SYSTEM_PROMPT}]
 
